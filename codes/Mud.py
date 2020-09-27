d8b0e8ccac6750eaf628eba93559ff2908a0b8e0
Sylvain Vanneste <vanneste@lal.in2p3.fr>
2020-09-28 01:57:11 +0200

    mud problem + color boxes

--- codes/Mud.py
+++ codes/Mud.py
@@ -15,10 +15,26 @@ from scipy import optimize
 plt.ion()
 plt.show()
 
-plt.figure()
+A = (0, 0)
+B = (5, -2)
+fig, ax = plt.subplots(figsize=[10, 5])
 plt.clf()
-plt.plot(0, 0, "ko")
+ax = plt.gca()
+# plt.plot(0, 0, "ko")
 plt.fill_between([-10, 10], 0, -1, color="C5", alpha=0.3)
-plt.plot(5, -2, "rs")
+# plt.plot(5, -2, "rs")
 plt.xlim(-3, 7)
 plt.ylim(-2.5, 0.5)
+plt.xlabel(r"x", fontweight='bold')
+plt.ylabel(r"y", fontweight='bold')
+
+
+arr_sleigh = mpimg.imread('images/sleigh.png')
+arr_igloo = mpimg.imread('images/igloo.png')
+ax.imshow(arr_sleigh, extent=[-0.2, 0.2, -0.0, 0.4])
+ax.imshow(arr_igloo, extent=[B[0]-0.2, B[0]+0.2, B[1]-0.0, B[1]+0.4])
+# Smashicons
+# Pixelmeetup
+plt.text(-2.5, -0.5, r"Mud", fontweight='bold')
+plt.text(-2.5, -1.7, r"Snow", fontweight='bold')
+plt.savefig("images/mud1.jpg")

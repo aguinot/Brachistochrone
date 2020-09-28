c3337ec36f55ec04c09e894b8c235ce8ac4fefa5
Sylvain Vanneste <vanneste@lal.in2p3.fr>
2020-09-28 15:38:44 +0200

    mud interact ok

--- codes/Mud.py
+++ codes/Mud.py
@@ -17,12 +17,15 @@ plt.show()
 
 A = (0, 0)
 B = (5, -2)
+
+
+#### static plot :
 fig, ax = plt.subplots(figsize=[10, 5])
 plt.clf()
 ax = plt.gca()
-# plt.plot(0, 0, "ko")
+plt.plot(0, 0, "ko")
 plt.fill_between([-10, 10], 0, -1, color="C5", alpha=0.3)
-# plt.plot(5, -2, "rs")
+plt.plot(5, -2, "rx")
 plt.xlim(-1, 6)
 plt.ylim(-2.3, 0.5)
 plt.xlabel(r"x", fontweight='bold')
@@ -38,3 +41,30 @@ ax.imshow(arr_igloo, extent=[B[0]-0.2, B[0]+0.2, B[1]-0.0, B[1]+0.4])
 plt.text(-0.9, -0.5, r"Mud", fontweight='bold')
 plt.text(-0.9, -1.7, r"Snow", fontweight='bold')
 plt.savefig("images/mud1.jpg")
+
+
+#### interactive plot :
+
+D = (0, -1)
+
+fig, ax = plt.subplots(figsize=[10, 5])
+plt.clf()
+ax = plt.gca()
+plt.plot(0, 0, "ko")
+plt.fill_between([-10, 10], 0, -1, color="C5", alpha=0.3)
+plt.plot(5, -2, "rx")
+plt.xlim(-1, 6)
+plt.ylim(-2.3, 0.5)
+plt.xlabel(r"x", fontweight='bold')
+plt.ylabel(r"y", fontweight='bold')
+arr_sleigh = mpimg.imread('images/sleigh.png')
+arr_igloo = mpimg.imread('images/igloo.png')
+ax.imshow(arr_sleigh, extent=[-0.2, 0.2, -0.0, 0.4])
+ax.imshow(arr_igloo, extent=[B[0]-0.2, B[0]+0.2, B[1]-0.0, B[1]+0.4])
+plt.text(-0.9, -0.5, r"Mud", fontweight='bold')
+plt.text(-0.9, -1.7, r"Snow", fontweight='bold')
+plt.plot([A[0], D[0]], [A[1], D[1]], color="C3", lw=2)
+plt.plot([D[0], B[0]], [D[1], B[1]], color="C2", lw=2)
+
+
+# plt.savefig("images/mud1.jpg")
